4d6d09ed659598a9aa55c7c2fab7f5507f25acf8
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-23 10:20:00 +0100

    test simple api

--- functions/videos/[id]?.js
+++ functions/videos/[id]?.js
@@ -1,7 +1,3 @@
-export const onRequestGet = () => {
-  return new Response("This is alist of videos!")
-}
-
 /*
 export const onRequestPost = async ({ request }) => {
   const { name } = await request.json()
@@ -9,44 +5,44 @@ export const onRequestPost = async ({ request }) => {
 }
 */
 
-  // import { getSignedStreamId } from "../../utils/cfStream"
+  import { getSignedStreamId } from "../../utils/cfStream"
 
-  // export async function onRequestGet(context) {
+  export async function onRequestGet(context) {
 
-  //   const {
-  //     env,
-  //     params,
-  //   } = context;
+    const {
+      env,
+      params,
+    } = context;
      
-  //     //const { id } = params
+      //const { id } = params
   
-  //     if (params.id) {
-  //         const res = await fetch(`https://dash.cloudflare.com/${env.CF_ACCOUNT_ID}/stream/videos/${params.id}`, {
-  //             method: "GET",
-  //             headers: {
-  //                 "Authorization": `Bearer ${env.CF_API_TOKEN_STREAM}`
-  //             }
-  //         })
+      if (params.id) {
+          const res = await fetch(`https://dash.cloudflare.com/${env.CF_ACCOUNT_ID}/stream/videos/${params.id}`, {
+              method: "GET",
+              headers: {
+                  "Authorization": `Bearer ${env.CF_API_TOKEN_STREAM}`
+              }
+          })
   
-  //         console.log('This is our response', res);
-  //         const video = (await res.json()).result
+          console.log('This is our response', res);
+          const video = (await res.json()).result
   
-  //         console.log('This is our video', video);
-  //         if (video.meta.visibility !== "public") {
-  //             return new Response(null, {status: 401})
-  //         }
+          console.log('This is our video', video);
+          if (video.meta.visibility !== "public") {
+              return new Response(null, {status: 401})
+          }
   
-  //         const signedId = await getSignedStreamId(id, env.CF_STREAM_SIGNING_KEY)
+          const signedId = await getSignedStreamId(id, env.CF_STREAM_SIGNING_KEY)
   
-  //         console.log('This is our signedID', signedId);
-  //         return new Response(JSON.stringify({
-  //             signedId: `${signedId}`
-  //         }), {
-  //             headers: {
-  //                 "content-type": "application/json"
-  //             }
-  //         })
-  //     }
+          console.log('This is our signedID', signedId);
+          return new Response(JSON.stringify({
+              signedId: `${signedId}`
+          }), {
+              headers: {
+                  "content-type": "application/json"
+              }
+          })
+      }
        /*else {
           const url = new URL(request.url)
           const res = await (await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.CF_ACCOUNT_ID}/stream?search=${url.searchParams.get("search") || ""}`, {
@@ -72,5 +68,5 @@ export const onRequestPost = async ({ request }) => {
           }))
           return new Response(JSON.stringify(videos), {headers: {"content-type": "application/json"}})
       }*/
-//  }
+  }
   
